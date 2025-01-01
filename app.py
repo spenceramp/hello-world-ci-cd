8c439190b8f03763c571a0d2f2d1b49ac5a4370b
spencer <spencerampofo@gmail.com>
2025-01-01 18:17:40 -0500

    feat: add newline end of file for flake8

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
+
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-    
